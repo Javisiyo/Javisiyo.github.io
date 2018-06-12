d58459d4aec8623b864125a353626c319ba25279
Javisiyo <36761406+Javisiyo@users.noreply.github.com>
2018-06-12 20:04:01 +0200

    Add files via upload

--- scripts/form-u1011.php
+++ scripts/form-u1011.php
@@ -27,8 +27,8 @@ $form = array(
 		'unknown_method' => 'Unknown server request method'
 	),
 	'email' => array(
-		'from' => 'muse@musefree.com',
-		'to' => 'muse@musefree.com'
+		'from' => 'javiermanzano312@gmail.com',
+		'to' => 'javiermanzano312@gmail.com'
 	),
 	'fields' => array(
 		'custom_U1012' => array(
